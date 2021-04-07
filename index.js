db390e11050d7884177947fca8b99448935468fc
Rosario <rmrodriguez1@uc.cl>
2021-04-07 17:08:18 -0400

    12

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-async function getEmployees() {
-    let response = await
-    fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul")
-    let data = await response.json()
-    return data;
-    }
-    getEmployees().then((data)=> console.log(data))
+fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul")
+.then(response => response.json())
+.then(data => displayEpisodes(data));
+
+
+function displayEpisodes(data){
+   var episodes = document.getElementById('episodes'); 
+   alert(data);
+}
