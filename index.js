e5028c77a4b2cc0cd694a1f8140e468e7af60231
Rosario <rmrodriguez1@uc.cl>
2021-04-07 15:51:52 -0400

    3

---
+++ index.js
@@ -0,0 +1,12 @@
+fetch("https://tarea-1-breaking-bad.herokuapp.com/api/")
+   .then(response => response.json())
+   .then(characters => showCharacters(characters.results));
+
+showCharacters = characters => {
+const charactersDiv = document.querySelector("#breaking-bad  characters");
+characters.forEach(character => {
+    const characterElement = document.createElement("p");
+    characterElement.innerText = 'Character Name: ${character.name}';
+    charactersDiv.append(characterElement);
+});
+}
