4e529abae07e3b39c28b2162791d1d7cab856fb1
Rosario <rmrodriguez1@uc.cl>
2021-04-07 16:28:55 -0400

    9

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ fetch("https://tarea-1-breaking-bad.herokuapp.com/api/")
    .then(characters => showCharacters(characters.results));
 
 showCharacters = characters => {
-const charactersDiv = document.querySelector(".breaking-bad-characters");
+const charactersDiv = document.querySelector("breaking-bad-characters");
 characters.forEach(character => {
     const characterElement = document.createElement("p");
     characterElement.innerText = 'Character Name: ${character.name}';
     charactersDiv.append(characterElement);
 });
 }
+
+
+
+// https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul
