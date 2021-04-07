e6a01ead481a2b3ae0b1c58893cff855f7b8dba4
Rosario <rmrodriguez1@uc.cl>
2021-04-07 15:54:59 -0400

    5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ fetch("https://tarea-1-breaking-bad.herokuapp.com/api/")
    .then(characters => showCharacters(characters.results));
 
 showCharacters = characters => {
-const charactersDiv = document.querySelector("#breaking-bad  characters");
+const charactersDiv = document.querySelector("#breaking-bad-characters");
 characters.forEach(character => {
     const characterElement = document.createElement("p");
     characterElement.innerText = 'Character Name: ${character.name}';
