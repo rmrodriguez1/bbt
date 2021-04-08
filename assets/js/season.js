b169baea34456ec1ce7e4469fb83a74c14d72634
Rosario <rmrodriguez1@uc.cl>
2021-04-08 17:14:43 -0400

    2

--- assets/js/season.js
+++ assets/js/season.js
@@ -6,6 +6,6 @@ fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Cal
 function displayEpisodes(data){
    var episodes = document.getElementById('episodes'); 
    for (var i=0; i<data.length; i++){
-       episodes.append("Episode "+data[i].episode+": "+data[i].title+"\r\n");
+       episodes.append("Episode "+data[i].episode+": "+data[i].title);
    }
 }
