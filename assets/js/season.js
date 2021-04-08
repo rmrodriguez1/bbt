df50e987a6d5031a48c0cb2c6115fce3f093b2a4
Rosario <rmrodriguez1@uc.cl>
2021-04-08 18:54:45 -0400

    12

--- assets/js/season.js
+++ assets/js/season.js
@@ -1,3 +1,4 @@
+
 fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul")
 .then(response => response.json())
 .then(data => displayEpisodes(data));
@@ -6,6 +7,8 @@ fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Cal
 function displayEpisodes(data){
    var episodes = document.getElementById('episodes'); 
    for (var i=0; i<data.length; i++){
+       if (data[i].season == season){
        episodes.append("Episode "+data[i].episode+": "+data[i].title);
+       }
    }
 }
