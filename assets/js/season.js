be661e1f534bb2f1d6a651bcc54b348deabd429c
Rosario <rmrodriguez1@uc.cl>
2021-04-08 16:45:15 -0400

    5

--- assets/js/season.js
+++ assets/js/season.js
@@ -1,4 +1,4 @@
-fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=breaking+bad")
+fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul")
 .then(response => response.json())
 .then(data => displayEpisodes(data));
 
