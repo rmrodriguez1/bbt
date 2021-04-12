3b362f6a870d0a57050ca8f46b322ec2f53e0584
Rosario <rmrodriguez1@uc.cl>
2021-04-11 23:55:36 -0400

    100

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,29 @@
+window.onload = (event) => {
+    fetchInfo("Breaking+Bad", "seasons_bb");
+    fetchInfo("Better+Call+Saul", "seasons_bcs");
+};
+
+function fetchInfo(series, id){
+    fetch("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=" + series)
+        .then(response => response.json())
+        .then(data => { return displayInfo(data, series, id) });
+}
+
+function displayInfo(data, series, id) {
+
+    var N = data[data.length - 1].season;
+
+    var seasons = document.getElementById(id); 
+    for (var i=0; i<N; i++)
+    {
+        var li = document.createElement('li');
+        var newNode = document.createElement('a');
+        newNode.href = "./views/season.html?series=" + series + "&season=" + String(i + 1);
+        newNode.innerText = "Season " + String(i + 1);
+        li.appendChild(newNode)
+        seasons.appendChild(li);
+    }
+}
 
 function search()
 {
